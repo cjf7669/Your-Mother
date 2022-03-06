55aa3e91b785a2bf2d172fbea79001e37b708366
FarmBoy522 <80296197+FarmBoy522@users.noreply.github.com>
2022-03-06 00:05:04 -0500

    Updates to Model
    Added Setters & Getters and Started to add code for a loss bet case.

--- Client/CardsConfig.java
+++ Client/CardsConfig.java
@@ -2,5 +2,6 @@ package Client;
 
 public class CardsConfig {
 
+	
 
 }
--- Client/Model.java
+++ Client/Model.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Model {
 
     private final List<Observer< Model, Object >> observers;
+	private int accountBal, betAmount;
 
     public Model(){
         this.observers= new LinkedList<>();
     }
+	
     //TODO EVERYTHINGGGGGG
     public void addObserver( Observer< Model, Object > obs ) {
         this.observers.add( obs );
@@ -20,4 +22,28 @@ public class Model {
             obs.update( this, arg );
         }
     }
+	
+	public void lossBet() {
+		//if bet is lost 
+		
+		
+	}
+	
+	///---Setters&Getters---///
+	public int getBallance() {
+		return accountBal;		
+	}
+	
+	public void setBallance(int temp) {
+		accountBal = temp;
+	}
+	
+	public int getBet() {
+		return betAmount;
+	}
+	
+	public void setBet(int temp) {
+		betAmount = temp;
+	}
+		
 }

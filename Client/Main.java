ccbd7fd474af16d3b297f84e61947bfbff6e377a
Connor <cjf7669@g.rit.edu>
2022-03-05 23:39:08 -0500

    Background GUI work

--- Client/Main.java
+++ Client/Main.java
@@ -1,10 +1,15 @@
 package Client;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /*
 Main class that runs for the client devices to connect to the server
@@ -13,10 +18,17 @@ public class Main extends Application
         implements Observer<Model, Object> {
 
     private static final Model cardModel = new Model();
-    private final int HEIGHT = 750;
-    private final int WIDTH = 1500;
+    private final int HEIGHT = 600;
+    private final int WIDTH = 1000;
     private Stage stage;
     private BorderPane window;
+    private Label diningDollars;
+    private Scene beforeDeal;
+    private Scene afterDeal;
+    private FlowPane beforeDealButtonPanel;
+    private BorderPane beforeDealWindow;
+    private Background background;
+    private Color green;
 
     /**
      *
@@ -24,7 +36,10 @@ public class Main extends Application
     @Override
     public void init(){
         window = new BorderPane();
-        //update(cardModel, null);
+        beforeDealButtonPanel = new FlowPane();
+        beforeDealWindow = new BorderPane();
+        green = Color.web("#057520");
+        background = new Background(new BackgroundFill(green, CornerRadii.EMPTY, Insets.EMPTY));
         cardModel.addObserver(this);
     }
 
@@ -35,7 +50,7 @@ public class Main extends Application
     public void start(Stage stage){
         this.stage = stage;
         stage.setTitle("RIT BlackJack");
-        stage.setScene(sceneCreator());
+        stage.setScene(sceneCreatorBeforeDeal());
         stage.setWidth(WIDTH);
         stage.setHeight(HEIGHT);
         stage.setResizable(false);
@@ -44,13 +59,35 @@ public class Main extends Application
     }
 
 ////////////////////////////////////////////////////////////////////
-    public Scene sceneCreator(){
-        Scene scene = new Scene(window);
-        scene.setFill(Color.web("#057520"));
-        return scene;
+    public Scene sceneCreatorAfterDeal(){
+        Label moneyLabel = playerBalanceLabel();
+        window.setTop(moneyLabel);
+        window.setAlignment(moneyLabel,Pos.TOP_RIGHT);
+
+        window.setBackground(background);
+        afterDeal = new Scene(window);
+        return afterDeal;
     }
 
+    public Label playerBalanceLabel(){
+        diningDollars = new Label("$3000"); //TODO Change to reflect accurate player balance amount
+        diningDollars.setAlignment(Pos.TOP_RIGHT);
+        return diningDollars;
+    }
 
+    public Scene sceneCreatorBeforeDeal(){
+        Button plus = new Button("+");
+        Button minus = new Button("-");
+        Button bet = new Button("Bet");
+        beforeDealButtonPanel.getChildren().addAll(plus, minus, bet);
+        beforeDealWindow.setBottom(beforeDealButtonPanel);
+        beforeDealWindow.setAlignment(beforeDealButtonPanel, Pos.BOTTOM_CENTER);
+
+        Label betAmount = new Label("Bet amount: $50"); //TODO ADD BET AMOUNT INCREMENTATION
+
+        beforeDeal = new Scene(beforeDealWindow);
+        return beforeDeal;
+    }
 
 ////////////////////////////////////////////////////////////////////
 

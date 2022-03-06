395fd40a8565c4280b2e98903d88c363bee94366
Connor <cjf7669@g.rit.edu>
2022-03-05 22:01:07 -0500

    Background scene set

--- Client/Main.java
+++ Client/Main.java
@@ -3,15 +3,18 @@ package Client;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /*
-
+Main class that runs for the client devices to connect to the server
  */
 public class Main extends Application
         implements Observer<Model, Object> {
 
     private static final Model cardModel = new Model();
+    private final int HEIGHT = 750;
+    private final int WIDTH = 1500;
     private Stage stage;
     private BorderPane window;
 
@@ -32,10 +35,24 @@ public class Main extends Application
     public void start(Stage stage){
         this.stage = stage;
         stage.setTitle("RIT BlackJack");
-        stage.setScene(new Scene(window));
+        stage.setScene(sceneCreator());
+        stage.setWidth(WIDTH);
+        stage.setHeight(HEIGHT);
+        stage.setResizable(false);
         stage.show();
         update(cardModel, null);
     }
+
+////////////////////////////////////////////////////////////////////
+    public Scene sceneCreator(){
+        Scene scene = new Scene(window);
+        scene.setFill(Color.web("#057520"));
+        return scene;
+    }
+
+
+
+////////////////////////////////////////////////////////////////////
 
     /**
      *
